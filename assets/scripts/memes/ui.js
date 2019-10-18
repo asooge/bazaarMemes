70d0452055b08f7a36c5d84cf9779a6d0164ab86
asooge <asooge@gmail.com>
2019-10-18 16:42:27 -0400

    Updated handlebars template
    Meme-stream handlebars template updated to create html to input data
    to update each meme. Data-id is assigned equal to the meme-id. Need
    to update the updatePreview function to correspond the id of the image
    with the id of the meme so each meme can be updated and previewed in
    real time.

--- assets/scripts/memes/ui.js
+++ assets/scripts/memes/ui.js
@@ -13,6 +13,16 @@ const displayMemes = function (memeData) {
 const refreshMemes = function () {
   $('#meme-stream').children().remove()
   $('#meme-stream').append(showMemesTemplate({ memes: store.memes }))
+  memeEventListeners()
+}
+
+const memeEventListeners = function () {
+  $('.top-text').on('input', updatePreview)
+  $('.bottom-text').on('input', updatePreview)
+  $('.top-text-size').on('input', updatePreview)
+  $('.bottom-text-size').on('input', updatePreview)
+  $('.image-url').on('input', updatePreview)
+  $('.image-preview').on('error', badImagePreview)
 }
 
 const removeMeme = function (index) {
@@ -35,15 +45,16 @@ const badImagePreview = function (event) {
 
 const updatePreview = function (event) {
   console.log(event.target.id)
-  if (event.target.id === 'top-text') {
+  console.log(event.target.dataset.id)
+  if (event.target.id === 'top-text0') {
     $('#top-text-preview').text(event.target.value)
-  } else if (event.target.id === 'bottom-text') {
+  } else if (event.target.id === 'bottom-text0') {
     $('#bottom-text-preview').text(event.target.value)
-  } else if (event.target.id === 'top-text-size') {
+  } else if (event.target.id === 'top-text-size0') {
     $('#top-text-preview-size').css('font-size', `${event.target.value}vw`)
-  } else if (event.target.id === 'bottom-text-size') {
+  } else if (event.target.id === 'bottom-text-size0') {
     $('#bottom-text-preview-size').css('font-size', `${event.target.value}vw`)
-  } else if (event.target.id === 'image-url') {
+  } else if (event.target.id === 'image-url0') {
     $('#image-preview').attr('src', event.target.value)
   }
 }
